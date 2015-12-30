692081172668ad5a75956f28913052c656d2c794
DanisFabric <danisfabric@gmail.com>
2015-12-30 14:18:11 +0800

    Initial Commit

---
+++ RainbowNavigationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainbowNavigationSample
+//
+//  Created by Danis on 15/12/30.
+//  Copyright © 2015年 danis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
