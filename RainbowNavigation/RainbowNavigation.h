ba0ce65dbeff21b1f6704956b3b6c7f34cff33f8
DanisFabric <danisfabric@gmail.com>
2015-12-30 14:19:53 +0800

    create project

---
+++ RainbowNavigation/RainbowNavigation.h
@@ -0,0 +1,19 @@
+//
+//  RainbowNavigation.h
+//  RainbowNavigation
+//
+//  Created by Danis on 15/12/30.
+//  Copyright © 2015年 danis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RainbowNavigation.
+FOUNDATION_EXPORT double RainbowNavigationVersionNumber;
+
+//! Project version string for RainbowNavigation.
+FOUNDATION_EXPORT const unsigned char RainbowNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RainbowNavigation/PublicHeader.h>
+
+
